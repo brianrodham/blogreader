323d8312afc82c820faa407360d5552f03962a2d
Brian Rodham <brian.j.rodham@gmail.com>
2019-08-30 20:57:46 -0600

    Added missing project

---
+++ BlogReader/RSSFeedReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace BlogReader
+{
+    public interface IRSSFeedReader
+    {
+        SyndicationFeed GetFeedData(string url);
+    }
+    public class RSSFeedReader : IRSSFeedReader
+    {
+        public SyndicationFeed GetFeedData(string url){
+            var feed = XmlReader.Create(url);
+            return SyndicationFeed.Load(feed);
+        }
+    }
+}
